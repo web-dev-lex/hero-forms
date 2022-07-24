2221a62603c4701a05a9b0a0339fd1520ee96b16
Lex <webdevlex35@gmail.com>
2022-07-24 18:54:45 -0400

    template form complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Template-Based Contact Form Example';
+  title = 'Contact Form';
 
   onSubmit(form: NgForm) {
     console.log('Your form data : ', form.value);
